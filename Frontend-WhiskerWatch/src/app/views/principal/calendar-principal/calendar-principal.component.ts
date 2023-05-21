f010e7c924d3b25b5d87be319fe5b77716f4b48c
Rocio <rpalaofernandez@gmail.com>
2023-05-21 18:00:23 +0200

    add evento view list

--- Frontend-WhiskerWatch/src/app/views/principal/calendar-principal/calendar-principal.component.ts
+++ Frontend-WhiskerWatch/src/app/views/principal/calendar-principal/calendar-principal.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit, Output } from '@angular/core';
 })
 export class CalendarPrincipalComponent{
   public actualEvent!: string;
+  public newDate: string;
+  public newTask: string;
+
+  constructor(){
+    this.newDate = '';
+    this.newTask = '';
+  }
 
   public changeSelectedEvent(event: string): void {
     this.actualEvent = event;
+  }
+
+  public sendNewEvent(): void {
+
+  }
+
+  public dateIsCorrect(): Boolean {
+    let transformedNewDate = new Date(this.newDate);
+    let actualDate = new Date();
+    if(this.newTask.length == 0 || transformedNewDate <= actualDate) return false;
+    return true;
   }
 
 }
